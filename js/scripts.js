b65e8b915586a0fd6cde41190610d4631f3128b2
Kevin Kennedy <kkennedyy1@gmail.com>
2021-02-06 11:20:13 -0600

    Week 4 Assignment

--- js/scripts.js
+++ js/scripts.js
@@ -11,4 +11,10 @@ $(function() {
             $("#carouselButton").children("i").addClass("fa-pause");
         }
     });
+    $("#loginButton").click(function(){
+        $("#loginModal").modal("show");
+    });
+    $("#reserveButton").click(function(){
+        $("#reserveModal").modal("show");
+    });
 });
